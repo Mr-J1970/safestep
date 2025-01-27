12eed3e526bb3ed8a6aa68ee4f7fac03ae302fbb
Mr-J1970 <swarajks93@gmail.com>
2025-01-27 10:08:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -165,7 +165,7 @@ style.innerHTML = `
         border-radius: 8px;
         padding: 16px;
         margin: 16px;
-        background-color: #fff;
+        background-color: black;
         box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
         transition: transform 0.3s, box-shadow 0.3s;
     }
